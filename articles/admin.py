4d645e50bf0d2eb471ff30681089f4429f10f74a
Julius Carl Concepcion <carlconcepcion03@gmail.com>
2024-01-22 01:50:03 +0800

    add slug feature and its unit test

--- articles/admin.py
+++ articles/admin.py
@@ -3,7 +3,7 @@ from .models import Article
 
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'timestamp', 'updated']
+    list_display = ['id', 'title', 'slug', 'timestamp', 'updated']
     search_fields = ['title', 'content']
 
 admin.site.register(Article, ArticleAdmin)
--- articles/models.py
+++ articles/models.py
@@ -1,11 +1,14 @@
+
 from django.db import models
+from django.db.models.signals import pre_save, post_save
 from django.utils import timezone
-from django.utils.text import slugify
+
+from .utils import slugify_instance_title
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=120)
-    slug = models.SlugField(blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -14,8 +17,24 @@ class Article(models.Model):
     def save(self, *args, **kwargs):
         # obj = Article.objes.get(id=1)
         # set something
-        if self.slug is None:
-            self.slug = slugify(self.title)
+        # if self.slug is None:
+        #     self.slug = slugify(self.title)
         super().save(*args, **kwargs)
         # obj.save()
         # do another something
+            
+def article_pre_save(sender, instance, *args, **kwargs):
+    print('pre_save')
+    
+    if instance.slug is None:
+        slugify_instance_title(instance, save=False)
+    
+
+pre_save.connect(article_pre_save, sender=Article)
+
+def article_post_save(sender, instance, created, *args, **kwargs):
+    print('post_save')
+    if created:
+         slugify_instance_title(instance, save=True)
+
+post_save.connect(article_post_save, sender=Article)
